5bab56e8ab24c8718779f67fe6a0ce15843f6857
Leandro Nanni <nannimatias@gmail.com>
2023-10-25 14:27:53 -0300

    Funcionalidad añadida
    Se agregó la función en el archivo sells.js para tener la página funcionando.
    
    Se corrigió el footer en la nueva
    página.

--- assets/js/sells.js
+++ assets/js/sells.js
@@ -1,2 +1,21 @@
 function BuySurprise(){
+    let elemtoHTML = document.getElementById("inputCount");
+    let ticketsCount = elemtoHTML.value;
+    elemtoHTML = document.getElementById("inputType");
+    let type = elemtoHTML.value;
+    let price = 0;
+
+    switch(type){
+        case "Estudiante":
+            price = (1000*ticketsCount)*0.5;
+            break;
+        case "Jubilado":
+            price = (1000*ticketsCount)*0.25;
+            break;
+        case "Adulto":
+            price = (1000*ticketsCount)*0.9;
+            break;
+    }
+    elemtoHTML = document.getElementById("outputPrice");
+    elemtoHTML.value = "Total a pagar: $"+ price
 }
